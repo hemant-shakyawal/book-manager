d864db9604d592399999d3a371effc87158243e9
Hemant kumar shakyawal <hemantkumarshakyawal@gmail.com>
2025-04-19 13:36:10 +0530

    book mannager project created

---
+++ book.api/book.api/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using book.api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace book.api.Controllers
+{
+    // https://localhost:7025/api/books
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController:ControllerBase
+    {
+        private Book[] _books = new Book[]
+        {
+            new Book{Id = 1,Author = "Author One",Title = "Book One"},
+            new Book{Id=2,Author="Author Two",Title="Book Two"},
+             new Book{Id= 3,Author="Author Three",Title="Book Three"},
+
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books);
+        }
+
+    }
+}
---
+++ book.api/book.api/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace book.api.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
